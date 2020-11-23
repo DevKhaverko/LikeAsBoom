7687b398aa3e72adb503f4a858a43cec7e0833ca
DevKhaverko <khaveko@mail.ru>
2020-11-23 20:24:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/likeasboom/ui/dashboard/RecommendationsFragment.kt
@@ -0,0 +1,31 @@
+package com.example.likeasboom.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.likeasboom.R
+
+class RecommendationsFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: RecommendationsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProvider(this).get(RecommendationsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_recommendations, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/likeasboom/ui/dashboard/RecommendationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.likeasboom.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RecommendationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
